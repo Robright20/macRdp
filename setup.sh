a60e274286c3fe5710658170c75ac2067ba30f13
Robert Bright <robright28@gmail.com>
2021-11-12 16:07:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,8 +24,8 @@ echo $AMI_PEM | sudo tee /Users/alone/.ami.pem
 sudo chmod g-w,g-r /Users/alone/.ami.pem
 sudo chmod o-w,o-r /Users/alone/.ami.pem
 
-echo "Recovering data from the backup"
-rsync -aP -e "ssh -i /Users/alone/.ami.pem" $4 /Users/alone/
+echo "Recovering data from $4"
+rsync -aPv -e "ssh -i /Users/alone/.ami.pem" $4 /Users/alone/
 
 #echo "copying install_zsh script"
 #sudo cp -rf ./install_zsh /Users/alone/
