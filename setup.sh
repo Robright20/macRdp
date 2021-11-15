a158a46cb37e253b8f5722ddec61034644d0916e
Robert Bright <robright28@gmail.com>
2021-11-15 06:54:01 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,16 +25,18 @@ sudo chmod g-w,g-r /Users/alone/.ami.pem
 sudo chmod o-w,o-r /Users/alone/.ami.pem
 sudo chown alone /Users/alone/.ami.pem
 
+echo "Saving the aws_ec2 instance fingerprint"
 sudo mkdir /Users/alone/.ssh
 curl $AWS_EC2 | sudo tee /Users/alone/.ssh/known_hosts > /dev/null
 sudo chown alone /Users/alone/.ssh
 sudo chown alone /Users/alone/.ssh/known_hosts
 
 echo "save the backup source"
-echo "export BACKUP_SRC=$4" | sudo tee -a /Users/alone/.zshrc
+echo "export BACKUP_SRC=$4" | sudo tee -a /Users/alone/.zshrc > /dev/null
 
+echo "initializing user session"
 sudo cp -rf ./init-user /Users/alone
-echo 'source ~/init-user' | sudo tee -a /Users/alone/.zshrc
+echo 'source ~/init-user' | sudo tee -a /Users/alone/.zshrc > /dev/null
 
 sudo chown alone /Users/alone/.zshrc
 
